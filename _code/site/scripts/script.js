7ec3768e6a4678c77d782755d91c9cd1966b7477
Radu Balaban <radu-b@users.noreply.github.com>
2023-10-22 17:35:53 +0300

    Updates email.

--- _code/site/scripts/script.js
+++ _code/site/scripts/script.js
@@ -1,26 +1,22 @@
 $(function () {
+  $("#portfolioItemCarrousel").owlCarousel({
+    navigation: true,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true,
+  });
 
-    $('#portfolioItemCarrousel').owlCarousel({
-        navigation: true,
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true
-    });
+  var address = "dapper_wheeled0y@icloud.com";
 
-    var address = 'contact@';
-    address += 'carmenbalaban.com';
+  $("a.email-address")
+    .attr("href", "mailto:" + address)
+    .append(address);
 
-    $('a.email-address')
-        .attr('href', 'mailto:' + address)
-        .append(address);
+  $(".contact-form")
+    .attr("action", "//formspree.io/" + address)
+    .bootstrapValidator();
 
-    $('.contact-form')
-        .attr('action', '//formspree.io/' + address)
-        .bootstrapValidator();
-
-
-    if (window.location.href.indexOf('sent=1') > 0) {
-        $('.contact-sent').show();
-    }
-
+  if (window.location.href.indexOf("sent=1") > 0) {
+    $(".contact-sent").show();
+  }
 });
